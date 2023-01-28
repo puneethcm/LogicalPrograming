89bac815a2a92a41f39168d3250edaa42a3f090a
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-28 11:45:06 +0530

    Prime Numbers

---
+++ LogicalPrograming/PrimeNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LogicalPrograming
+{
+	public class PrimeNumbers
+	{
+        public static void PrimeNumber()
+        {
+            int factor = 0;
+            Console.WriteLine("\nEnter the numbers to find Prime");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    factor++;
+                }
+            }
+            if (factor == 2)
+            {
+                Console.WriteLine("\n{0} is Prime Numbner", n);
+            }
+            else
+            {
+                Console.WriteLine("\n{0} is Not a Prime Number", n);
+            }
+        }
+    }
+}
+
--- LogicalPrograming/Program.cs
+++ LogicalPrograming/Program.cs
@@ -5,13 +5,17 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Logical Programing Problems ");
-        Console.WriteLine("\n1:Fibonacci numbers");
+        Console.WriteLine("\n1:Fibonacci numbers\n2:Prime Numbers");
         int option = Convert.ToInt32(Console.ReadLine());
 
         switch (option)
         {
             case 1:
                 Fibonacci.FindFibonacciNumber();
+                break;
+
+            case 2:
+                PrimeNumbers.PrimeNumber();
                 break;
         }
         Console.ReadLine();
