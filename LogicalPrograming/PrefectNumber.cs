703c66dd352c2c6a8ab6d54c01e9facb41c67ae9
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-28 11:53:00 +0530

    Prefect Numbers

---
+++ LogicalPrograming/PrefectNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LogicalPrograming
+{
+	public class PrefectNumber
+	{
+		public static void FindPrefectNumber()
+		{
+			int sum = 0;
+			Console.WriteLine("\nEnter the Number");
+			int number = Convert.ToInt32(Console.ReadLine());
+			for(int i = 1; i < number; i++)
+			{
+				if (number % i == 0)
+				{
+					sum = sum + i;
+				}
+			}
+			if (number == sum)
+				Console.WriteLine("Its Prefect number");
+			else
+				Console.WriteLine("Its Not a Prefect number");
+		}
+	}
+}
+
--- LogicalPrograming/Program.cs
+++ LogicalPrograming/Program.cs
@@ -5,7 +5,7 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Logical Programing Problems ");
-        Console.WriteLine("\n1:Fibonacci numbers\n2:Prime Numbers");
+        Console.WriteLine("\n1:Fibonacci numbers\n2:Prime Numbers\n3:Prefect Number");
         int option = Convert.ToInt32(Console.ReadLine());
 
         switch (option)
@@ -16,6 +16,10 @@ internal class Program
 
             case 2:
                 PrimeNumbers.PrimeNumber();
+                break;
+
+            case 3:
+                PrefectNumber.FindPrefectNumber();
                 break;
         }
         Console.ReadLine();
