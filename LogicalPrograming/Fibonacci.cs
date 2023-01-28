61a5dfaef901c90534f162aa74c9019412d8700b
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-28 11:39:25 +0530

    Fibonacci numbers

---
+++ LogicalPrograming/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LogicalPrograming
+{
+	public class Fibonacci
+	{
+		public static void FindFibonacciNumber()
+		{
+            Console.WriteLine("\nEnter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int n1 = 0, n2 = 1, n3, i;
+            Console.Write("\nFibonacci Numbers are: ");
+            Console.Write(n1 + "" + n2 + "");
+
+            for (i = 2; i < number; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + "");
+                    //initializing n1 value to n2
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+	}
+}
+
--- LogicalPrograming/Program.cs
+++ LogicalPrograming/Program.cs
@@ -1,8 +1,19 @@
-﻿internal class Program
+﻿using LogicalPrograming;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Logical Programing Problems ");
+        Console.WriteLine("\n1:Fibonacci numbers");
+        int option = Convert.ToInt32(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+                Fibonacci.FindFibonacciNumber();
+                break;
+        }
         Console.ReadLine();
     }
 }
