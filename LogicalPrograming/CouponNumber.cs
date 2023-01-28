c83b96440938d4fc9e9d50853ffbf6a5a5cc1ba9
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-28 12:40:13 +0530

    Coupon number

---
+++ LogicalPrograming/CouponNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace LogicalPrograming
+{
+	public class CouponNumber
+	{
+		public static void GetCouponNumber()
+		{
+			int count = 0;
+			int arrCount = 0;
+			int[] arr = {10,32,42,65,29};
+
+			Random random = new Random();
+			while (arrCount != arr.Length)
+			{
+				int input = random.Next(0, 100);
+				count++;
+				switch (input)
+				{
+					case 10:
+						arr[0] = 10;
+						arrCount++;
+						break;
+                    case 32:
+                        arr[1] = 32;
+                        arrCount++;
+                        break;
+                    case 42:
+                        arr[2] = 42;
+                        arrCount++;
+                        break;
+                    case 65:
+                        arr[3] = 65;
+                        arrCount++;
+                        break;
+                    case 29:
+                        arr[4] = 29;
+                        arrCount++;
+                        break;
+                }
+			}
+			Console.WriteLine("\nCoupon Number are: ");
+			for(int i = 0; i < arr.Length; i++)
+			{
+				Console.Write("{0}, ", (arr[i]));
+			}
+            Console.WriteLine("\nRandom number count: " + count + " in order Generate all distinct coupon numbers");
+        }
+	}
+}
+
--- LogicalPrograming/Program.cs
+++ LogicalPrograming/Program.cs
@@ -5,7 +5,7 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Logical Programing Problems ");
-        Console.WriteLine("\n1:Fibonacci numbers\n2:Prime Numbers\n3:Prefect Number\n4:Reverse Number");
+        Console.WriteLine("\n1:Fibonacci numbers\n2:Prime Numbers\n3:Prefect Number\n4:Reverse Number\n5:Coupon Number");
         int option = Convert.ToInt32(Console.ReadLine());
 
         switch (option)
@@ -24,6 +24,10 @@ internal class Program
 
             case 4:
                 ReverseNumber.FindReverseNumber();
+                break;
+
+            case 5:
+                CouponNumber.GetCouponNumber();
                 break;
         }
         Console.ReadLine();
