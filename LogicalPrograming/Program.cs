14ab9de98b737916ed3db97147beef94b5df052f
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-28 10:25:51 +0530

    [Puneeth] Logical Programing

---
+++ LogicalPrograming/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Logical Programing Problems ");
+        Console.ReadLine();
+    }
+}
