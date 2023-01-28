be5d5f5e1979b10fcfda5c4c6ed8c3a951775bd8
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-28 12:00:31 +0530

    Reverse Number

--- LogicalPrograming/Program.cs
+++ LogicalPrograming/Program.cs
@@ -5,7 +5,7 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Logical Programing Problems ");
-        Console.WriteLine("\n1:Fibonacci numbers\n2:Prime Numbers\n3:Prefect Number");
+        Console.WriteLine("\n1:Fibonacci numbers\n2:Prime Numbers\n3:Prefect Number\n4:Reverse Number");
         int option = Convert.ToInt32(Console.ReadLine());
 
         switch (option)
@@ -20,6 +20,10 @@ internal class Program
 
             case 3:
                 PrefectNumber.FindPrefectNumber();
+                break;
+
+            case 4:
+                ReverseNumber.FindReverseNumber();
                 break;
         }
         Console.ReadLine();
---
+++ LogicalPrograming/ReverseNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LogicalPrograming
+{
+	public class ReverseNumber
+	{
+		public static void FindReverseNumber()
+		{
+			int remainder = 0, reverse = 0;
+			Console.WriteLine("\nEnter the number to be Reversed");
+			int number = Convert.ToInt32(Console.ReadLine());
+			while (number != 0)
+			{
+				remainder = number % 10;
+				reverse = (reverse * 10) + remainder;
+				number = number / 10;
+			}
+			Console.WriteLine("\nReverse number: " + reverse);
+		}
+	}
+}
+
